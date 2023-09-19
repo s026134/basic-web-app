4bef63618cb03b7d73277a0b57e37669ae075f90
Your Name <s026134@students.lmsd.org>
2023-09-19 16:16:37 -0400

    minus query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -39,6 +39,15 @@ export default function QueryProcessor(query: string): string {
           return newString;
     }
   }
+  else if(query.toLowerCase().includes("minus")){
+    const addMatch = query.match(/What is (\d+) minus (\d+)/);
+    if (addMatch) {
+          const x: number = parseInt(addMatch[1]);
+          const y: number = parseInt(addMatch[2]);
+          const newString : string = (x - y).toString();
+          return newString;
+    }
+  }
   else if(query.toLowerCase().includes("largest")){
     const addMatch = query.match(/Which of the following numbers is the largest: (\d+), (\d+), (\d+)/);
     if (addMatch) {
@@ -49,19 +58,22 @@ export default function QueryProcessor(query: string): string {
           return newString;
     }
   }
-  else if(query.toLowerCase().includes("square and a cube")){
-    const addMatch = query.match(/Which of the following numbers is both a square and a cube: (\d+), (\d+), (\d+), (\d+), (\d+), (\d+), (\d+)/);
-    if (addMatch) {
-          const x: number = parseInt(addMatch[1]);
-          const y: number = parseInt(addMatch[2]);
-          const z: number = parseInt(addMatch[3]);
-          const a: number = parseInt(addMatch[4]);
-          const b: number = parseInt(addMatch[5]);
-          const c: number = parseInt(addMatch[6]);
-          const newString : string = (Math.max(x, y ,z)).toString();
-          return `The answer is ${newString}`;
-    }
-  }
+  // else if(query.toLowerCase().includes("square and a cube")){
+  //   const addMatch = query.match(/Which of the following numbers is both a square and a cube: (\d+), (\d+), (\d+), (\d+), (\d+), (\d+), (\d+)/);
+  //   if (addMatch) {
+  //         const x: number = parseInt(addMatch[1]);
+  //         const y: number = parseInt(addMatch[2]);
+  //         const z: number = parseInt(addMatch[3]);
+  //         const a: number = parseInt(addMatch[4]);
+  //         const b: number = parseInt(addMatch[5]);
+  //         const c: number = parseInt(addMatch[6]);
+  //         const array = [x, y, z, a, b, c];
+  //         array.forEach(element => {
+  //           if(Math.pow(element, 1/6) == )
+  //         });
+  //         return `The answer is ${newString}`;
+  //   }
+  // }
 
   return "";
 }
